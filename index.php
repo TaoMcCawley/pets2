d2d11e930f52f47baaa3d21b2f9c363dcd8f0129
jmcpherson9@mail.greenriver.edu <jmcphers@srv603.webhostingforstudents.com>
2018-01-18 14:27:21 -0500

    added second route to the color form

--- index.php
+++ index.php
@@ -34,7 +34,9 @@ $f3->route('GET /order', function(){
 });
 
 $f3->route('GET /order2', function(){
-   echo "<p>Form 2</p>";
+   $template = new Template();
+   echo $template->render('views/form2.html');
+
 });
 $f3->route('GET /results', function(){
     echo "<p>Results</p>";
