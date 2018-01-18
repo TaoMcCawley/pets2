5ec8a17b4befd46a92c579be2482ee7b4a80e217
Adrian Smith <adriansmith5796@gmail.com>
2018-01-18 14:32:34 -0500

    Stores form1 info in session

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  */
 
 require 'vendor/autoload.php';
+session_start();
 
 $f3 = Base::instance();
 
@@ -33,9 +34,10 @@ $f3->route('GET /order', function(){
     echo $template->render('views/form1.html');
 });
 
-$f3->route('GET /order2', function(){
-   $template = new Template();
-   echo $template->render('views/form2.html');
+$f3->route('POST /order2', function(){
+    $_SESSION['animal'] = $_POST['animal'];
+    $template = new Template();
+    echo $template->render('views/form2.html');
 
 });
 $f3->route('GET /results', function(){
