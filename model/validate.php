547c0519658793b9911b97d86cea8592bfb934c6
jmcpherson9@mail.greenriver.edu <jmcphers@srv603.webhostingforstudents.com>
2018-01-26 14:11:29 -0500

    Added valid string function

--- model/validate.php
+++ model/validate.php
@@ -8,3 +8,9 @@ function validColor($color) {
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
+
+function validString($string){
+    if(!is_string($string) && ctype_alpha($string)){
+        return false;
+    }
+}
